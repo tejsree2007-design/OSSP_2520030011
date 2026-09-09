435d0d3d86cd955bec0e7de35c1a994805df4d76
tejsree2007-design <tejsree2007@gmail.com>
2026-09-09 12:22:19 +0530

    Create socketclient.c

---
+++ Practical/socketclient.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
+#define SOCKET_PATH "/tmp/my_socket"
+int main(){
+    int client_fd;
+    struct sockaddr_un address;
+
+    client_fd = socket(AF_UNIX,SOCK_STREAM,0);
+
+    address.sun_family = AF_UNIX;
+    strcpy(address.sun_path, SOCKET_PATH);
+
+    connect(client_fd,
+            (struct sockaddr *)&address,
+            sizeof(address));
+
+    write(client_fd, "Hello Server",12);
+    printf("Message sent to server.\n");
+    close(client_fd);
+    return 0;
+}
